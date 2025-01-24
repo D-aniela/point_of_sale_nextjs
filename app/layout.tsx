0791a63ba2c4082dcf35261519ef733886e63516
Laura Daniela Estrada Ibarra <daniela.es.ib@gmail.com>
2025-01-24 16:17:11 -0600

    POS, show products and get categories

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Outfit } from 'next/font/google'
+import './globals.css'
+
+const outfit = Outfit({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Next.js POS',
+  description: 'A point of sale system built with Next.js',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang='es'>
+      <body className={`${outfit.className} bg-gray-200`}>{children}</body>
+    </html>
+  )
+}
