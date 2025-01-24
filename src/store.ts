3f07ef80481fb341004d6e60272c54042c69afdd
Laura Daniela Estrada Ibarra <daniela.es.ib@gmail.com>
2025-01-24 16:41:55 -0600

    state duplicates

--- src/store.ts
+++ src/store.ts
@@ -12,6 +12,47 @@ export const useStore = create<Store>()(
   devtools((set, get) => ({
     total: 0,
     contents: [],
-    addToCart: (product) => {},
+    addToCart: (product) => {
+      const { id: productId, categoryId, ...data } = product
+      let contents: ShoppingCart = []
+      const duplicated = get().contents.findIndex(
+        (item) => item.productId === productId
+      )
+      if (duplicated >= 0) {
+        if (
+          get().contents[duplicated].quantity >=
+          get().contents[duplicated].stock
+        )
+          return
+
+        contents = get().contents.map((item) =>
+          item.productId === productId
+            ? {
+                ...item,
+                quantity: item.quantity + 1,
+              }
+            : item
+        )
+      } else {
+        contents = [
+          /**
+           * Get. toma contents del state
+           */
+          ...get().contents,
+          {
+            ...data,
+            quantity: 1,
+            productId,
+          },
+        ]
+      }
+
+      /**
+       * Set. actualiza contents en el state
+       */
+      set(() => ({
+        contents,
+      }))
+    },
   }))
 )
