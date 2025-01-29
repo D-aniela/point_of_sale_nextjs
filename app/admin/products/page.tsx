79b56a28926f4f38f7b74d6cb23bde97c9da5eb3
Laura Daniela Estrada Ibarra <daniela.es.ib@gmail.com>
2025-01-28 21:25:08 -0600

    get products

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,7 +1,20 @@
-export default function ProductsPage() {
+import Heading from '@/components/ui/Heading'
+import { ProductsResponseSchema } from '@/src/schemas'
+
+async function getProducts() {
+  const url = `${process.env.API_URL}/products`
+  const req = await fetch(url)
+  const json = await req.json()
+  const products = ProductsResponseSchema.parse(json)
+  return products
+}
+
+export default async function ProductsPage() {
+  const products = await getProducts()
+
   return (
-    <div>
-      <p>Products Page</p>
-    </div>
+    <>
+      <Heading>Administrar productos</Heading>
+    </>
   )
 }
